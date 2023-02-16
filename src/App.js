bc514cf36bb19220ac4e5fa0e8a9f21db4a78da5
sarjunenp <sarjunenp@gmail.com>
2023-02-16 17:44:05 +0800

    Updated Changes

--- src/App.js
+++ src/App.js
@@ -144,7 +144,6 @@ const App = ({ signOut }) => {
               {note.name}
             </Text>
             <Text as="span">{note.description}</Text>
-            <Text as="span">{note.contact}</Text>
             {note.image && (
               <Image
                 src={note.image}
